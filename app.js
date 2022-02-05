c994c87f85cf6c2e0cb32b72a3fcf69a7414b2da
Brendan Urban <bsurban7@gmail.com>
2022-02-05 00:21:31 -0800

    Help me coding gods

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ function yourName() {
     return name;
 }
 
-function guessANumber() {
-    let answer;
-
-    do {
-        answer = prompt('Guess a number between 1-10');
-        if (answer != 3) {
-            alert('try again!')
-        } else {
-            alert('you are correct!');
-        }
-
-    } while (answer != 3)
-}
-
 function timeOfDay() {
     let time = prompt('What time is it?'); // 15;
     let message = '';
